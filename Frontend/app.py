34971f22382146c64ba30df44f07cd672c5d43ee
yqianguo <e0906433@u.nus.edu>
2024-04-26 13:40:37 +0800

    Change some errors.

--- Frontend/app.py
+++ Frontend/app.py
@@ -92,4 +92,4 @@ app.layout = dcc.Loading(  # <- Wrap App with Loading Component
 server = app.server
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run(host='0.0.0.0', port=8050)
